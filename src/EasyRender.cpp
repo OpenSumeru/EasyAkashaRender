ba0706717d3b0b96c3e3f22af63073d0d8e8f5f3
tabye320@outlook.com <TabNahida>
2024-01-09 18:17:41 +0800

    render object

---
+++ src/EasyRender.cpp
@@ -0,0 +1,32 @@
+#include "../include/EasyRender.hpp"
+using namespace EasyRender;
+
+EasyWindow::EasyWindow()
+{
+    
+}
+    
+EasyWindow::~EasyWindow()
+{
+
+}
+
+EasyPage::EasyPage()
+{
+
+}
+    
+EasyPage::~EasyPage()
+{
+    
+}
+
+EasyRenderObject::EasyRenderObject()
+{
+
+}
+    
+EasyRenderObject::~EasyRenderObject()
+{
+
+}
